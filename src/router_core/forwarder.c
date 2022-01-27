fe9bba1df2da46afe6b27346319fc5f5d08ae595
Kenneth Giusti <kgiusti@apache.org>
2022-01-27 15:39:39 -0500

    ISSUE-86: fix TSAN errors involving the uptime counter
    DISPATCH-2132: atomic access to core uptime counter
    
    This closes #86

--- src/router_core/forwarder.c
+++ src/router_core/forwarder.c
@@ -135,7 +135,7 @@ qdr_delivery_t *qdr_forward_new_delivery_CT(qdr_core_t *core, qdr_delivery_t *in
 {
     qdr_delivery_t *out_dlv = new_qdr_delivery_t();
     if (out_link->conn)
-        out_link->conn->last_delivery_time = core->uptime_ticks;
+        out_link->conn->last_delivery_time = qdr_core_uptime_ticks(core);
 
     ZERO(out_dlv);
     set_safe_ptr_qdr_link_t(out_link, &out_dlv->link_sp);
@@ -156,7 +156,7 @@ qdr_delivery_t *qdr_forward_new_delivery_CT(qdr_core_t *core, qdr_delivery_t *in
         }
     } else {
         out_dlv->settled       = true;
-        out_dlv->ingress_time  = core->uptime_ticks;
+        out_dlv->ingress_time  = qdr_core_uptime_ticks(core);
         out_dlv->ingress_index = -1;
     }
 
