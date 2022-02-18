282db4f72ce37760905be78ed42bfb98ae44837c
Jiri Daněk <jdanek@redhat.com>
2022-02-18 20:21:07 +0100

    Update STATIC_ASSERT macro with C11 features

--- include/qpid/dispatch/static_assert.h
+++ include/qpid/dispatch/static_assert.h
@@ -20,6 +20,8 @@
  * under the License.
  */
 
+#include <assert.h>
+
 /** @file
  * STATIC_ASSERT allows you to do compile time assertions at file scope or in a function.
  * @param expr: a boolean expression that is valid at compile time.
@@ -27,16 +29,7 @@
  */
 
 
-#ifdef __GNUC__
-#define STATIC_ASSERT_HELPER(expr, msg) \
-    (!!sizeof(struct { unsigned int STATIC_ASSERTION__##msg: (expr) ? 1 : -1; }))
-#define STATIC_ASSERT(expr, msg) \
-    extern int (*assert_function__(void)) [STATIC_ASSERT_HELPER(expr, msg)]
-#else
-    #define STATIC_ASSERT(expr, msg)   \
-    extern char STATIC_ASSERTION__##msg[1]; \
-    extern char STATIC_ASSERTION__##msg[(expr)?1:2]
-#endif /* #ifdef __GNUC__ */
+#define STATIC_ASSERT(expr, msg) static_assert(expr, #msg)
 
 #define STATIC_ASSERT_ARRAY_LEN(array, len) \
     STATIC_ASSERT(sizeof(array)/sizeof(array[0]) == len, array##_wrong_size);
