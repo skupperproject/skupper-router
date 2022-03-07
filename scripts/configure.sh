aa14921909fd215001d226f8d35e0a7959e5c4f3
Fernando Giorgetti <fgiorget@redhat.com>
2022-03-07 15:59:37 -0300

    Fixed #133: ensure no unbound variables

--- scripts/configure.sh
+++ scripts/configure.sh
@@ -29,10 +29,10 @@ function printConfig() {
     echo "---------------------------------------" && cat $OUTFILE && echo "---------------------------------------"
 }
 
-if [[ $QDROUTERD_CONF =~ .*\{.*\}.* ]]; then
+if [[ ${QDROUTERD_CONF:-} =~ .*\{.*\}.* ]]; then
     # env var contains inline config
     echo "$QDROUTERD_CONF" > $OUTFILE
-elif [[ -n $QDROUTERD_CONF ]]; then
+elif [[ -n ${QDROUTERD_CONF:-} ]]; then
     # treat as path(s)
     IFS=':,' read -r -a array <<< "$QDROUTERD_CONF"
     > $OUTFILE
@@ -56,10 +56,10 @@ if [ -f $OUTFILE ]; then
     python3 $HOME_DIR/bin/expandvars.py $OUTFILE
 fi
 
-if [ -n "$QDROUTERD_AUTO_MESH_DISCOVERY" ]; then
+if [ -n "${QDROUTERD_AUTO_MESH_DISCOVERY:-}" ]; then
     python3 $HOME_DIR/bin/auto_mesh.py $OUTFILE || printConfig
 fi
 
-if [ -n "$QDROUTERD_AUTO_CREATE_SASLDB_SOURCE" ]; then
+if [ -n "${QDROUTERD_AUTO_CREATE_SASLDB_SOURCE:-}" ]; then
     $HOME_DIR/bin/create_sasldb.sh ${QDROUTERD_AUTO_CREATE_SASLDB_PATH:-$HOME_DIR/etc/qdrouterd.sasldb} $QDROUTERD_AUTO_CREATE_SASLDB_SOURCE "${APPLICATION_NAME:-qdrouterd}"
 fi
