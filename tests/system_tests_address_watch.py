de9f05385f0304b9ece5ce166e9a1210a3b9cd11
Ted Ross <tross@apache.org>
2022-04-25 11:58:28 -0400

    Fixed #376 - Updated edge_addr_proxy to proxy watched addresses. Added missing test cases for edge-address-watch

--- tests/system_tests_address_watch.py
+++ tests/system_tests_address_watch.py
@@ -31,13 +31,16 @@ class RouterTest(TestCase):
         """Start a router"""
         super(RouterTest, cls).setUpClass()
 
-        def router(name, connection):
+        def router(name, connection, connection2=None, mode='interior'):
 
             config = [
-                ('router', {'mode': 'interior', 'id': name}),
+                ('router', {'mode': mode, 'id': name}),
                 ('listener', {'port': cls.tester.get_port()}),
                 connection
             ]
+
+            if connection2:
+                config.append(connection2)
 
             config = Qdrouterd.Config(config)
 
@@ -46,25 +49,49 @@ class RouterTest(TestCase):
         cls.routers = []
 
         inter_router_port = cls.tester.get_port()
+        edge_port         = cls.tester.get_port()
 
-        router('A', ('listener', {'role': 'inter-router', 'port': inter_router_port}))
+        router('A', ('listener', {'role': 'inter-router', 'port': inter_router_port}),
+                    ('listener', {'role': 'edge', 'port': edge_port}))
         router('B', ('connector', {'name': 'connectorToA', 'role': 'inter-router', 'port': inter_router_port}))
+        router('C', ('connector', {'name': 'connectorToA', 'role': 'edge', 'port': edge_port}), mode='edge')
 
         cls.routers[0].wait_router_connected('B')
         cls.routers[1].wait_router_connected('A')
 
-    def test_address_watch(self):
-        test = AddressWatchTest(self.routers[0], self.routers[1])
+    def test_01_interior_interior(self):
+        test = AddressWatchTest(self.routers[0], self.routers[1], 0)
+        test.run()
+        self.assertIsNone(test.error)
+
+    def test_02_edge_interior(self):
+        test = AddressWatchTest(self.routers[2], self.routers[0], 1)
+        test.run()
+        self.assertIsNone(test.error)
+
+    def test_03_interior_edge(self):
+        test = AddressWatchTest(self.routers[0], self.routers[2], 2)
+        test.run()
+        self.assertIsNone(test.error)
+
+    def test_04_edge_far_interior(self):
+        test = AddressWatchTest(self.routers[2], self.routers[1], 3)
+        test.run()
+        self.assertIsNone(test.error)
+
+    def test_05_far_interior_edge(self):
+        test = AddressWatchTest(self.routers[1], self.routers[2], 4)
         test.run()
         self.assertIsNone(test.error)
 
 
 class AddressWatchTest(MessagingHandler):
-    def __init__(self, host_a, host_b):
+    def __init__(self, host_a, host_b, index):
         super(AddressWatchTest, self).__init__()
         self.host_a = host_a
         self.host_b = host_b
-        self.addr     = 'addr_watch/test_address/1'
+        self.index  = index
+        self.addr     = 'addr_watch/test_address/%d' % index
         self.conn_a   = None
         self.conn_b   = None
         self.error    = None
@@ -96,16 +123,16 @@ class AddressWatchTest(MessagingHandler):
         if self.n_closed == 2:
             with open(self.host_a.logfile_path, 'r') as router_log:
                 log_lines = router_log.read().split("\n")
-                search_lines = [s for s in log_lines if "ADDRESS_WATCH" in s and "on_watch" in s]
-                matches = [s for s in search_lines if "loc: 1 rem: 0 prod: 0" in s]
+                search_lines = [s for s in log_lines if "ADDRESS_WATCH" in s and "on_watch(%d)" % self.index in s]
+                matches = [s for s in search_lines if ("loc: 1 rem: 0 prod: 0" in s) or ("loc: 1 rem: 0 prod: 1" in s)]
                 if len(matches) == 0:
-                    self.error = "Didn't see local consumer on router A"
+                    self.error = "Didn't see local consumer on router 1"
             with open(self.host_b.logfile_path, 'r') as router_log:
                 log_lines = router_log.read().split("\n")
-                search_lines = [s for s in log_lines if "ADDRESS_WATCH" in s and "on_watch" in s]
-                matches = [s for s in search_lines if "loc: 0 rem: 1 prod: 1" in s]
+                search_lines = [s for s in log_lines if "ADDRESS_WATCH" in s and "on_watch(%d)" % self.index in s]
+                matches = [s for s in search_lines if ("loc: 0 rem: 1 prod: 1" in s) or ("loc: 1 rem: 0 prod: 1" in s)]
                 if len(matches) == 0:
-                    self.error = "Didn't see remote consumer and local producer on router B"
+                    self.error = "Didn't see remote consumer and local producer on router 2"
             self.timer.cancel()
 
     def run(self):
