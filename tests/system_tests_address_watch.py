5627671f9f77fdb28e987e58a813207a67d8a068
Ted Ross <tross@apache.org>
2022-05-05 09:05:42 -0400

    Fixes #425 - Added new tests that use the new test-hooks.

--- tests/system_tests_address_watch.py
+++ tests/system_tests_address_watch.py
@@ -20,6 +20,7 @@
 from system_test import TestCase, Qdrouterd, main_module, TIMEOUT, unittest, TestTimeout, retry_exception
 from proton.handlers import MessagingHandler
 from proton.reactor import Container
+from proton import Message
 
 
 class RouterTest(TestCase):
@@ -55,6 +56,7 @@ class RouterTest(TestCase):
                     ('listener', {'role': 'edge', 'port': edge_port}))
         router('B', ('connector', {'name': 'connectorToA', 'role': 'inter-router', 'port': inter_router_port}))
         router('C', ('connector', {'name': 'connectorToA', 'role': 'edge', 'port': edge_port}), mode='edge')
+        router('D', ('connector', {'name': 'connectorToA', 'role': 'edge', 'port': edge_port}), mode='edge')
 
         cls.routers[0].wait_router_connected('B')
         cls.routers[1].wait_router_connected('A')
@@ -82,6 +84,86 @@ class RouterTest(TestCase):
 
     def test_05_far_interior_edge(self):
         test = AddressWatchTest(self.routers[1], self.routers[2], 4)
+        test.run()
+        self.assertIsNone(test.error)
+
+    def test_06_dynamic_same_interior(self):
+        test = DynamicAddressWatchTest('test_06', self.routers[0], [self.routers[0]], 1, 0)
+        test.run()
+        self.assertIsNone(test.error)
+
+    def test_07_dynamic_interior_interior(self):
+        test = DynamicAddressWatchTest('test_07', self.routers[0], [self.routers[1]], 0, 1)
+        test.run()
+        self.assertIsNone(test.error)
+
+    def test_08_dynamic_two_interiors(self):
+        test = DynamicAddressWatchTest('test_08', self.routers[0], [self.routers[0], self.routers[1]], 1, 1)
+        test.run()
+        self.assertIsNone(test.error)
+
+    def test_09_dynamic_two_interiors_multiple_local(self):
+        test = DynamicAddressWatchTest('test_09', self.routers[0], [self.routers[0], self.routers[1], self.routers[0]], 2, 1)
+        test.run()
+        self.assertIsNone(test.error)
+
+    def test_10_dynamic_same_edge(self):
+        test = DynamicAddressWatchTest('test_10', self.routers[2], [self.routers[2]], 1, 0)
+        test.run()
+        self.assertIsNone(test.error)
+
+    def test_11_dynamic_edge_interior(self):
+        test = DynamicAddressWatchTest('test_11', self.routers[2], [self.routers[0]], 1, 0)
+        test.run()
+        self.assertIsNone(test.error)
+
+    def test_12_dynamic_edge_interior_both(self):
+        test = DynamicAddressWatchTest('test_12', self.routers[2], [self.routers[0], self.routers[2]], 2, 0)
+        test.run()
+        self.assertIsNone(test.error)
+
+    def test_13_dynamic_edge_far_interior(self):
+        test = DynamicAddressWatchTest('test_13', self.routers[2], [self.routers[1]], 1, 0)
+        test.run()
+        self.assertIsNone(test.error)
+
+    def test_14_dynamic_edge_all_interior(self):
+        test = DynamicAddressWatchTest('test_14', self.routers[2], [self.routers[1], self.routers[0]], 1, 0)
+        test.run()
+        self.assertIsNone(test.error)
+
+    def test_15_dynamic_edge_all_interior_multiple_local(self):
+        test = DynamicAddressWatchTest('test_15', self.routers[2], [self.routers[1], self.routers[0], self.routers[2]], 2, 0)
+        test.run()
+        self.assertIsNone(test.error)
+
+    def test_16_dynamic_interior_edge(self):
+        test = DynamicAddressWatchTest('test_16', self.routers[0], [self.routers[2]], 1, 0)
+        test.run()
+        self.assertIsNone(test.error)
+
+    def test_17_dynamic_interior_edge_both(self):
+        test = DynamicAddressWatchTest('test_17', self.routers[0], [self.routers[2], self.routers[0]], 2, 0)
+        test.run()
+        self.assertIsNone(test.error)
+
+    def test_18_dynamic_far_interior_edge(self):
+        test = DynamicAddressWatchTest('test_18', self.routers[1], [self.routers[2]], 0, 1)
+        test.run()
+        self.assertIsNone(test.error)
+
+    def test_19_dynamic_far_interior_edge_both(self):
+        test = DynamicAddressWatchTest('test_19', self.routers[1], [self.routers[2], self.routers[1]], 1, 1)
+        test.run()
+        self.assertIsNone(test.error)
+
+    def test_20_dynamic_edge_edge(self):
+        test = DynamicAddressWatchTest('test_20', self.routers[2], [self.routers[3]], 1, 0)
+        test.run()
+        self.assertIsNone(test.error)
+
+    def test_21_dynamic_edge_edge_local(self):
+        test = DynamicAddressWatchTest('test_21', self.routers[2], [self.routers[3], self.routers[2]], 2, 0)
         test.run()
         self.assertIsNone(test.error)
 
@@ -126,13 +208,13 @@ class AddressWatchTest(MessagingHandler):
                 with open(self.host_a.logfile_path, 'r') as router_log:
                     log_lines = router_log.read().split("\n")
                     search_lines = [s for s in log_lines if "ADDRESS_WATCH" in s and "on_watch(%d)" % self.index in s]
-                    matches = [s for s in search_lines if ("loc: 1 rem: 0 prod: 0" in s) or ("loc: 1 rem: 0 prod: 1" in s)]
+                    matches = [s for s in search_lines if "loc: 1 rem: 0" in s]
                     if len(matches) == 0:
                         raise Exception("Didn't see local consumer on router 1")
                 with open(self.host_b.logfile_path, 'r') as router_log:
                     log_lines = router_log.read().split("\n")
                     search_lines = [s for s in log_lines if "ADDRESS_WATCH" in s and "on_watch(%d)" % self.index in s]
-                    matches = [s for s in search_lines if ("loc: 0 rem: 1 prod: 1" in s) or ("loc: 1 rem: 0 prod: 1" in s)]
+                    matches = [s for s in search_lines if ("loc: 0 rem: 1" in s) or ("loc: 1 rem: 0" in s)]
                     if len(matches) == 0:
                         raise Exception("Didn't see remote consumer and local producer on router 2")
 
@@ -140,6 +222,101 @@ class AddressWatchTest(MessagingHandler):
             # Try repeatedly until TIMEOUT seconds.
             retry_exception(check_log_lines, delay=2)
             self.timer.cancel()
+
+    def run(self):
+        Container(self).run()
+
+
+class DynamicAddressWatchTest(MessagingHandler):
+    def __init__(self, address, watch_host, dest_hosts, expected_local, expected_remote):
+        super(DynamicAddressWatchTest, self).__init__()
+        self.address = address
+        self.watch_host = watch_host
+        self.dest_hosts = dest_hosts
+        self.expected_local  = expected_local
+        self.expected_remote = expected_remote
+
+        self.conn_watch = None
+        self.conn_dests = []
+        self.error      = None
+        self.sender     = None
+        self.receiver   = None
+        self.dest_receivers = []
+        self.phase = "START"
+
+    def fail(self, error=None):
+        self.error = error
+        if self.conn_watch:
+            self.conn_watch.close()
+        for conn in self.conn_dests:
+            conn.close()
+        self.timer.cancel()
+
+    def timeout(self):
+        self.fail("Timeout Expired - Phase: %s" % self.phase)
+
+    def setup_dests(self):
+        for conn in self.conn_dests:
+            self.dest_receivers.append(self.container.create_receiver(conn, self.address))
+
+    def close_dests(self):
+        for rx in self.dest_receivers:
+            rx.close()
+        self.dest_receivers = []
+
+    def on_start(self, event):
+        self.container  = event.container
+        self.timer      = event.reactor.schedule(10.0, TestTimeout(self))
+        self.conn_watch = event.container.connect(self.watch_host.addresses[0])
+        self.receiver   = event.container.create_receiver(self.conn_watch, "_local/_testhook/watch_event")
+        for host in self.dest_hosts:
+            self.conn_dests.append(self.container.connect(host.addresses[0]))
+
+    def on_link_opened(self, event):
+        if event.receiver == self.receiver:
+            self.sender = event.container.create_sender(self.conn_watch, "_local/_testhook/address_watch")
+
+    def on_sendable(self, event):
+        if self.phase == "START":
+            self.phase = "SET-WATCH"
+            msg = Message(subject='watch', properties={'opcode': 'watch-on', 'address': self.address})
+            self.sender.send(msg)
+
+    def on_accepted(self, event):
+        if self.phase == "UNWATCH":
+            self.fail(None)
+
+    def on_message(self, event):
+        msg = event.message
+        ap = msg.properties
+        local  = ap['local_consumers']
+        remote = ap['remote_consumers']
+        addr   = ap['address']
+
+        if addr != self.address:
+            self.fail("Received a watch for an unexpected address:  Expected %s, got %s" % (self.address, addr))
+
+        ##print("phase=%s, local=%d, remote=%d" % (self.phase, local, remote))
+
+        if self.phase == "SET-WATCH":
+            if local == 0 and remote == 0:
+                self.phase = "WATCHING"
+                self.setup_dests()
+            else:
+                self.fail("Expected 0 consumers, got local=%d, remote=%d" % (local, remote))
+
+        elif self.phase == "WATCHING":
+            if local == self.expected_local and remote == self.expected_remote:
+                self.phase = "CLEAR-WATCH"
+                self.close_dests()
+
+        elif self.phase == "CLEAR-WATCH":
+            if local == 0 and remote == 0:
+                self.phase = "UNWATCH"
+                msg = Message(subject='watch', properties={'opcode': 'watch-off', 'address': self.address})
+                self.sender.send(msg)
+            elif local > self.expected_local or remote > self.expected_remote:
+                self.fail("Exceeded expected counts: expected l=%d, r=%d; got l=%d, r=%d" % (self.expected_local, self.expected_remote, local, remote))
 
     def run(self):
         Container(self).run()
