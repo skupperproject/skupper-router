ec654963a834835ca2e3178c4809d442cee4216d
Ted Ross <tross@apache.org>
2022-11-09 15:02:34 -0500

    Moved edge-peer connection tracking from main core to edge-router module.

--- src/router_core/modules/edge_router/connection_manager.c
+++ src/router_core/modules/edge_router/connection_manager.c
@@ -46,6 +46,90 @@ struct qcm_edge_conn_mgr_t {
 };
 
 
+static qdr_edge_peer_t *qdr_find_edge_peer_CT(qdr_core_t *core, const char *container_id)
+{
+    qdr_edge_peer_t *edge_peer = DEQ_HEAD(core->edge_peers);
+
+    while (!!edge_peer) {
+        if (strcmp(container_id, edge_peer->identity) == 0) {
+            break;
+        }
+        edge_peer = DEQ_NEXT(edge_peer);
+    }
+
+    return edge_peer;
+}
+
+
+static void qdr_inter_edge_peer_activate_CT(qdr_core_t *core, qdr_edge_peer_t *edge_peer)
+{
+    edge_peer->router_addr = qdr_add_local_address_CT(core, QD_ITER_HASH_PREFIX_EDGE_SUMMARY, edge_peer->identity, QD_TREATMENT_ANYCAST_CLOSEST);
+    qdr_link_t *link = qdr_create_link_CT(core, edge_peer->primary_conn, QD_LINK_INTER_EDGE, QD_OUTGOING,
+                                          qdr_terminus_inter_edge(), qdr_terminus_inter_edge(), QD_SSN_ENDPOINT, QDR_DEFAULT_PRIORITY);
+    qdr_core_bind_address_link_CT(core, edge_peer->router_addr, link);
+}
+
+
+static void qdr_inter_edge_connection_setup_CT(qdr_core_t *core, qdr_connection_t *conn)
+{
+    qdr_edge_peer_t *edge_peer = qdr_find_edge_peer_CT(core, conn->connection_info->container);
+
+    if (!edge_peer) {
+        //
+        // This is the first connection we've seen from this edge-peer.  Create a new record for it.
+        //
+        edge_peer = NEW(qdr_edge_peer_t);
+        ZERO(edge_peer);
+        edge_peer->identity = strdup(conn->connection_info->container);
+        DEQ_INSERT_TAIL(core->edge_peers, edge_peer);
+        qd_log(core->log, QD_LOG_INFO, "Edge peer detected: %s", edge_peer->identity);
+        qd_iterator_add_peer_edge(edge_peer->identity);
+    }
+
+    //
+    // Add this connection to the list of connections associated with this edge-peer.
+    //
+    qdr_add_connection_ref(&edge_peer->connections, conn);
+    conn->edge_peer = edge_peer;
+
+    //
+    // If there is no primary connection for this edge-peer, use this one and activate.
+    //
+    if (edge_peer->primary_conn == 0) {
+        edge_peer->primary_conn = conn;
+        qdr_inter_edge_peer_activate_CT(core, edge_peer);
+        qdrc_event_conn_raise(core, QDRC_EVENT_CONN_MESH_PEER_ESTABLISHED, conn);
+    }
+}
+
+
+static void qdr_inter_edge_connection_cleanup_CT(qdr_core_t *core, qdr_connection_t *conn)
+{
+    qdr_edge_peer_t *edge_peer = conn->edge_peer;
+
+    if (!!edge_peer) {
+        qdr_del_connection_ref(&edge_peer->connections, conn);
+        if (DEQ_SIZE(edge_peer->connections) > 0) {
+            if (edge_peer->primary_conn == conn) {
+                qdrc_event_conn_raise(core, QDRC_EVENT_CONN_MESH_PEER_LOST, conn);
+                edge_peer->primary_conn = DEQ_HEAD(edge_peer->connections)->conn;
+                qdr_inter_edge_peer_activate_CT(core, edge_peer);
+                qdrc_event_conn_raise(core, QDRC_EVENT_CONN_MESH_PEER_ESTABLISHED, edge_peer->primary_conn);
+            }
+        } else {
+            qd_log(core->log, QD_LOG_INFO, "Edge peer lost: %s", edge_peer->identity);
+            qdrc_event_conn_raise(core, QDRC_EVENT_CONN_MESH_PEER_LOST, conn);
+            qd_iterator_del_peer_edge(edge_peer->identity);
+            DEQ_REMOVE(core->edge_peers, edge_peer);
+            edge_peer->router_addr->ref_count--;
+            qdr_check_addr_CT(core, edge_peer->router_addr);
+            free(edge_peer->identity);
+            free(edge_peer);
+        }
+    }
+}
+
+
 static void on_conn_event(void *context, qdrc_event_t event, qdr_connection_t *conn)
 {
     qcm_edge_conn_mgr_t *cm = (qcm_edge_conn_mgr_t*) context;
@@ -57,6 +141,14 @@ static void on_conn_event(void *context, qdrc_event_t event, qdr_connection_t *c
             cm->active_edge_connection = conn;
             cm->core->active_edge_connection = conn;
             qdrc_event_conn_raise(cm->core, QDRC_EVENT_CONN_EDGE_ESTABLISHED, conn);
+        }
+
+        if (conn->role == QDR_ROLE_INTER_EDGE) {
+            //
+            // This is an inter-edge connection.  Maintain a list of inter-edge connections indexed by the remote
+            // container-id.  Handle the case where there is more than one connection to the same peer edge.
+            //
+            qdr_inter_edge_connection_setup_CT(cm->core, conn);
         }
         break;
 
@@ -79,6 +171,14 @@ static void on_conn_event(void *context, qdrc_event_t event, qdr_connection_t *c
                        conn->identity);
                 cm->active_edge_connection = 0;
             }
+        }
+
+        //
+        // If this is an inter-edge connection, clean up references to this connection in the list
+        // of peer-edge routers.
+        //
+        if (conn->role == QDR_ROLE_INTER_EDGE) {
+            qdr_inter_edge_connection_cleanup_CT(cm->core, conn);
         }
         break;
 
