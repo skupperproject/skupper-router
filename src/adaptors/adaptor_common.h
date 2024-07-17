ad1965d85d50ff6109f62f35fac1f6067b60c202
Gabor Dozsa <gabor.dozsa@ibm.com>
2024-07-17 11:52:33 +0200

    Fixes #1563: Delete corresponding connections optionally on deletion of tcpListener and tcpConnector
    Signed-off-by: Gabor Dozsa <gabor.dozsa@ibm.com>

--- src/adaptors/adaptor_common.h
+++ src/adaptors/adaptor_common.h
@@ -56,6 +56,7 @@ struct qd_adaptor_config_t
     char              *site_id;
     char              *host_port;
     int                backlog;
+    bool               terminate_conns;
 
     //TLS related info
     char              *ssl_profile_name;
