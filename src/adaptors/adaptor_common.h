0ce661d54162466ebf16a1aa0800420b0e275645
Gabor Dozsa <gabor.dozsa@ibm.com>
2024-08-02 14:15:44 +0200

    Make the new config flag global to the router

--- src/adaptors/adaptor_common.h
+++ src/adaptors/adaptor_common.h
@@ -56,7 +56,6 @@ struct qd_adaptor_config_t
     char              *site_id;
     char              *host_port;
     int                backlog;
-    bool               terminate_conns;
 
     //TLS related info
     char              *ssl_profile_name;
