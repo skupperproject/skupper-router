e5925b5fd89364d178a36694a47f41aaa3374bd7
ganeshmurthy <gmurthy@apache.org>
2025-03-11 12:15:13 -0400

    Fixes #1756: Remove qd_node_type_t struct and replaced its callbacks with direct calls to AMQP functions

--- include/qpid/dispatch/amqp_adaptor.h
+++ include/qpid/dispatch/amqp_adaptor.h
@@ -35,7 +35,7 @@ typedef struct pn_link_t    pn_link_t;
 typedef struct qd_connection_t qd_connection_t;
 typedef struct qd_link_t       qd_link_t;
 typedef struct qd_session_t    qd_session_t;
-
+typedef struct qd_router_t     qd_router_t;
 
 // For use by message.c
 
@@ -53,4 +53,57 @@ size_t qd_session_get_outgoing_capacity(const qd_session_t *qd_ssn);
 // Used by the log module
 void qd_amqp_connection_set_tracing(bool enabled);
 
+
+//
+// AMQP handlers
+//
+/**
+ * Invoked when an attach for a new outgoing (sending) link is received.
+ */
+int  AMQP_outgoing_link_handler(qd_router_t *router, qd_link_t *link);
+/**
+ * Invoked when a new or existing received delivery is available for processing.
+ */
+bool AMQP_rx_handler(qd_router_t *router, qd_link_t *link);
+/**
+ * Invoked when an existing delivery changes disposition or settlement state.
+ */
+void AMQP_disposition_handler(qd_router_t *router, qd_link_t *link, pn_delivery_t *pnd);
+/**
+ * Invoked when an attach for a new incoming (receiving) link is received.
+ */
+int  AMQP_incoming_link_handler(qd_router_t *router, qd_link_t *link);
+/**
+ * Invoked when a link we created was opened by the peer
+ */
+int  AMQP_link_attach_handler(qd_router_t *router, qd_link_t *link);
+ /**
+  * Invoked when link detached is received.
+  */
+int  AMQP_link_detach_handler(qd_router_t *router, qd_link_t *link);
+ /**
+  * Invoked when an incoming connection (via listener) is opened.
+  */
+int  AMQP_inbound_opened_handler(qd_router_t *router, qd_connection_t *conn, void *context);
+/**
+ * Invoked when an outgoing connection (via connector) is opened.
+ */
+int  AMQP_outbound_opened_handler(qd_router_t *router, qd_connection_t *conn, void *context);
+/**
+ * Invoked when a connection is closed.
+ */
+int  AMQP_closed_handler(qd_router_t *router, qd_connection_t *conn, void *context);
+/**
+ * Invoked when an activated connection is available for writing.
+ */
+int  AMQP_conn_wake_handler(qd_router_t *router, qd_connection_t *conn, void *context);
+/** The last callback issued for the given qd_link_t. The adaptor must clean up all state related to the qd_link_t
+ * as it will be freed on return from this call. The forced flag is set to true if the link is being forced closed
+ * due to the parent connection/session closing or on shutdown.
+ */
+void AMQP_link_closed_handler(qd_router_t *router, qd_link_t *qd_link, bool forced);
+/**
+ * Invoked when a link receives a flow event
+ */
+int  AMQP_link_flow_handler(qd_router_t *router, qd_link_t *link);
 #endif
