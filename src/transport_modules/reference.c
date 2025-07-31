7e932b596752f6ed8cb03185fe7b083b922fd309
Ted Ross <tross@apache.org>
2025-07-31 12:38:38 -0400

    Began annotating the reference file with development instructions.

--- src/transport_modules/reference.c
+++ src/transport_modules/reference.c
@@ -17,6 +17,22 @@
  * under the License.
  */
 
+ //
+ // HOW TO USE THIS FILE:
+ //
+ // - Make a copy of this source file in the same directory but under a different name.
+ // - Add the new file to src/CMakeLists.txt so it will be built.
+ //
+ // In your copy:
+ //   - Change the name of the module in the module_name constant.
+ //   - Fill in all of the TODO sections with your specific implementation.
+ //
+ // To select your plugin as the active transport:
+ // Add the following line to the "router" section in etc/skrouterd.conf:
+ //
+ //     transportPlugin: <the name of your module>
+ //
+
 #include <qpid/dispatch/ctools.h>
 #include <qpid/dispatch/enum.h>
 #include <qpid/dispatch/alloc_pool.h>
@@ -24,6 +40,62 @@
 #include <qpid/dispatch/protocol_adaptor.h>
 #include <qpid/dispatch/log.h>
 #include <qpid/dispatch/adaptor_common.h>
+#include <qpid/dispatch/service_transport.h>
+
+//===========================================================================================
+// Settings and Definitions
+//===========================================================================================
+
+//
+// TODO: Change the name
+//
+static const char *module_name = "reference";
+
+//===========================================================================================
+// State Definitions
+//===========================================================================================
+
+//
+// Service Listener
+//
+typedef struct tmod_tcp_listener_t {
+    DEQ_LINKS(struct tmod_tcp_listener_t);
+} tmod_tcp_listener_t;
+
+ALLOC_DECLARE(tmod_tcp_listener_t);
+ALLOC_DEFINE(tmod_tcp_listener_t);
+DEQ_DECLARE(tmod_tcp_listener_t, tmod_tcp_listener_list_t);
+
+//
+// Service Connector
+//
+typedef struct tmod_tcp_connector_t {
+    DEQ_LINKS(struct tmod_tcp_connector_t);
+} tmod_tcp_connector_t;
+
+ALLOC_DECLARE(tmod_tcp_connector_t);
+ALLOC_DEFINE(tmod_tcp_connector_t);
+DEQ_DECLARE(tmod_tcp_connector_t, tmod_tcp_connector_list_t);
+
+//
+// Inter-Router Listener
+//
+
+//
+// Inter-Router Connector
+//
+
+//
+// Inter-Router Link
+//
+
+//
+// Module State
+//
+typedef struct {
+    tmod_tcp_listener_list_t  tcpListeners;
+    tmod_tcp_connector_list_t tcpConnectors;
+} tmod_module_t;
 
 //===========================================================================================
 // Transport API Handlers
@@ -80,15 +152,21 @@ static qd_error_t refresh_tcp_connector(qd_entity_t* entity, void *impl)
 //
 static void TRANSPORT_init(qdr_core_t *core, void **adaptor_context)
 {
-    qd_log(LOG_ROUTER, QD_LOG_INFO, "Reference Transport Module Initialized");
+    qd_log(LOG_ROUTER, QD_LOG_INFO, "Transport Module Initialized: %s", module_name);
 
-    qd_register_tcp_management_handlers(configure_tcp_listener,
-                                        configure_tcp_connector,
-                                        update_tcp_listener,
-                                        delete_tcp_listener,
-                                        delete_tcp_connector,
-                                        refresh_tcp_listener,
-                                        refresh_tcp_connector);
+    qd_register_tcp_management_handlers(
+        configure_tcp_listener,
+        configure_tcp_connector,
+        update_tcp_listener,
+        delete_tcp_listener,
+        delete_tcp_connector,
+        refresh_tcp_listener,
+        refresh_tcp_connector
+    );
+
+    tmod_module_t *module = NEW(tmod_module_t);
+    ZERO(module);
+    *adaptor_context = module;
 }
 
 //
@@ -96,10 +174,11 @@ static void TRANSPORT_init(qdr_core_t *core, void **adaptor_context)
 //
 static void TRANSPORT_final(void *module_context)
 {
-    qd_log(LOG_ROUTER, QD_LOG_INFO, "Reference Transport Module Finalized");
+    qd_log(LOG_ROUTER, QD_LOG_INFO, "Transport Module Finalized: %s", module_name);
+    free(module_context);
 }
 
 /**
  * Declare the module so that it will self-register on process startup.
  */
-QDR_TRANSPORT_MODULE_DECLARE("reference", TRANSPORT_init, TRANSPORT_final)
+QDR_TRANSPORT_MODULE_DECLARE(module_name, TRANSPORT_init, TRANSPORT_final)
